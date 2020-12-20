851c63101f027e5778ef0d3af3f4537dc9d2fa3d
omid-joukar <joukaromid6@gmail.com>
2020-12-20 10:00:46 +0100

    added visits. updated owners withot relationsheep.  Closes #26

--- pet-clinic-data/src/main/java/omid/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/omid/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,18 @@
 package omid.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by omid on 12/12/2020.
  */
 public class Owner extends Person {
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
