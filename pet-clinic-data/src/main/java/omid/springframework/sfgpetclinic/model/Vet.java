e932bb900bb5831b3db2b8c48ce8350cd88648ea
omid-joukar <joukaromid6@gmail.com>
2020-12-20 10:06:37 +0100

    added Speciality.updated vet entity.  Closes #27

--- pet-clinic-data/src/main/java/omid/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/omid/springframework/sfgpetclinic/model/Vet.java
@@ -1,7 +1,18 @@
 package omid.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by omid on 12/12/2020.
  */
 public class Vet extends Person {
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
