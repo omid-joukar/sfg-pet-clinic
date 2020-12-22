61ca6e8113b7377b33fa0342caa11c70f064a43b
omid-joukar <joukaromid6@gmail.com>
2020-12-22 15:56:45 +0100

    update vet entity with JPA.  Closes #37

--- pet-clinic-data/src/main/java/omid/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/omid/springframework/sfgpetclinic/model/Vet.java
@@ -1,12 +1,19 @@
 package omid.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by omid on 12/12/2020.
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties",
+    joinColumns = @JoinColumn(name = "vet_id"),
+    inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialities = new HashSet<>();
 
     public Set<Specialty> getSpecialities() {
