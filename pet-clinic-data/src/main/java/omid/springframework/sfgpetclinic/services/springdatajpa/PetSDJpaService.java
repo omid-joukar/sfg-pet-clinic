10dee327a7d2ffd237775ad87245911f121a0c78
omid-joukar <joukaromid6@gmail.com>
2020-12-22 21:56:37 +0100

    added spring data jpa service for pets.  Closes #43

---
+++ pet-clinic-data/src/main/java/omid/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,47 @@
+package omid.springframework.sfgpetclinic.services.springdatajpa;
+
+import omid.springframework.sfgpetclinic.model.Pet;
+import omid.springframework.sfgpetclinic.repositories.PetRepository;
+import omid.springframework.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService  implements PetService {
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delet(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
