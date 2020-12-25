d945a6d08d334dc37ebd86feeff1a647c1324632
omid-joukar <joukaromid6@gmail.com>
2020-12-25 12:28:17 +0100

    added MockMvc tests for owner controller.  Closes #52

---
+++ pet-clinic-web/src/test/java/omid/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,54 @@
+package omid.springframework.sfgpetclinic.controllers;
+
+import omid.springframework.sfgpetclinic.model.Owner;
+import omid.springframework.sfgpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+    @Mock
+    OwnerService ownerService;
+    @InjectMocks
+    OwnerController ownerController;
+    Set<Owner> owners;
+    MockMvc mockMvc;
+    @BeforeEach
+    void setUp() {
+        owners = new HashSet<>();
+        owners.add(Owner.builder().id(1L).build());
+        owners.add(Owner.builder().id(2L).build());
+        mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
+    }
+
+    @Test
+    void listOwners() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index" ))
+                .andExpect(model().attribute("owners",hasSize(2)));
+
+    }
+
+    @Test
+    void findOwners() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners/find")).
+                andExpect(status().isOk()).andExpect(view().name("notimplemented"));
+    }
+}
