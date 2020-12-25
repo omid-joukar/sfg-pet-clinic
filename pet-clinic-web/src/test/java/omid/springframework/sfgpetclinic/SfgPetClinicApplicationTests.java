3bea47998f4cbdeacf755dcb6cb8265e35fee108
omid-joukar <joukaromid6@gmail.com>
2020-12-25 09:37:05 +0100

    upgrade to Junit 5.  Closes #4

--- pet-clinic-web/src/test/java/omid/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/omid/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,12 +1,17 @@
 package omid.springframework.sfgpetclinic;
 
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = SfgPetClinicApplication.class)
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class SfgPetClinicApplicationTests {
+    @Test
+    public void contextLoads(){
 
+    }
 
 }
