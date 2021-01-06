5e5ef5ec740e28d8007af91bf87640a4944e3892
omid-joukar <joukaromid6@gmail.com>
2021-01-06 12:39:09 +0100

    added json endpoint. Closes #68

--- pet-clinic-web/src/main/java/omid/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/omid/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,10 +1,14 @@
 package omid.springframework.sfgpetclinic.controllers;
 
+import omid.springframework.sfgpetclinic.model.Vet;
 import omid.springframework.sfgpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,5 +22,9 @@ public class VetController {
     public String listVets(Model model){
         model.addAttribute("vets",vetService.findAll());
         return "vets/index";
+    }
+    @RequestMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
